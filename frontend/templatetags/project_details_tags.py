9b1d7589dd18d1ab3b2c2575c213521a977606b5
Raphael <raphael.robidas@usherbrooke.ca>
2020-08-11 20:42:27 -0400

    Optimized the project page

--- frontend/templatetags/project_details_tags.py
+++ frontend/templatetags/project_details_tags.py
@@ -4,34 +4,46 @@ from frontend.models import *
 register = template.Library()
 
 @register.simple_tag
-def get_molecule_summary(mol):
-    num = 0
-    completed = 0
-    queued = 0
-    running = 0
-    for o in mol.project.calculationorder_set.all():
-        if o.ensemble != None:
-            if o.ensemble.parent_molecule == mol:
-                num += o.calculation_set.count()
-                for c in o.calculation_set.all():
-                    s = c.status
-                    if s == 0:
-                        queued += 1
-                    elif s == 1:
-                        running += 1
-                    else:
-                        completed += 1
-        elif o.structure != None:
-            if o.structure.parent_ensemble.parent_molecule == mol:
-                num += o.calculation_set.count()
-                for c in o.calculation_set.all():
-                    s = c.status
-                    if s == 0:
-                        queued += 1
-                    elif s == 1:
-                        running += 1
-                    else:
-                        completed += 1
+def get_molecule_summary(molecules):
+    num = {}
+    completed = {}
+    queued = {}
+    running = {}
+    for mol in molecules:
+        num[mol.id] = 0
+        completed[mol.id] = 0
+        queued[mol.id] = 0
+        running[mol.id] = 0
 
-    return [num, queued, running, completed]
+    for o in molecules[0].project.calculationorder_set.all():
+        if o.ensemble != None:
+            #if o.ensemble.parent_molecule == mol:
+            mol = o.ensemble.parent_molecule
+            num[mol.id] += o.calculation_set.count()
+            for c in o.calculation_set.all():
+                s = c.status
+                if s == 0:
+                    queued[mol.id] += 1
+                elif s == 1:
+                    running[mol.id] += 1
+                else:
+                    completed[mol.id] += 1
+        elif o.structure != None:
+            mol = o.structure.parent_ensemble.parent_molecule
+            num[mol.id] += o.calculation_set.count()
+            for c in o.calculation_set.all():
+                s = c.status
+                if s == 0:
+                    queued[mol.id] += 1
+                elif s == 1:
+                    running[mol.id] += 1
+                else:
+                    completed[mol.id] += 1
+
+    pack = []
+    for mol in molecules:
+        pack.append([mol, [num[mol.id], queued[mol.id], running[mol.id], completed[mol.id]]])
+
+    return pack
+    #return [num, queued, running, completed]
 
