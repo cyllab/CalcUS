8c046a543b0147ca620a91caf15b6873d692e34a
Raphael <raphael.robidas@usherbrooke.ca>
2020-09-22 20:46:46 -0400

    Added equivalent proton detection using Morgan's algorithm (+tests)

--- frontend/libxyz.py
+++ frontend/libxyz.py
@@ -1,4 +1,6 @@
 import numpy as np
+import periodictable
+from .constants import *
 
 #Structure of xyz:
 #[<EL>, [x, y, z]]
@@ -31,7 +33,82 @@ def get_dihedral(xyz, a, b, c, d):
 
     return np.arctan2(y, x)*180/np.pi
 
+def parse_xyz(f):
+    """ Parses a standard .xyz file into a suitable structure for calculations """
+    with open(f) as ff:
+        lines = ff.readlines()[2:]
+    xyz = []
 
+    for line in lines:
+        a, x, y, z = line.split()
+        xyz.append([a, np.array([float(x), float(y), float(z)])])
 
+    return xyz
+
+def get_connectivity(xyz):
+    """ Returns a list of pairs of bonded atoms """
+    TOLERANCE = 1.1
+    bonds = []
+    def bond_unique(ind1, ind2):
+        for bond in bonds:
+            if bond[0] == ind1 and bond[1] == ind2:
+                return False
+            if bond[0] == ind2 and bond[1] == ind1:
+                return False
+        return True
+
+    for ind1, i in enumerate(xyz):
+        for ind2, j in enumerate(xyz):
+            if ind1 > ind2:
+                d = get_distance(xyz, ind1+1, ind2+1)
+                cov = (periodictable.elements[ATOMIC_NUMBER[i[0]]].covalent_radius + periodictable.elements[ATOMIC_NUMBER[j[0]]].covalent_radius)
+
+                if d < cov*TOLERANCE and bond_unique(ind1, ind2):
+                    bonds.append([ind1, ind2])
+    return bonds
+
+def get_neighbors_lists(xyz):
+    """ Returns a list neighbors (bonded atoms) for each atom """
+    bonds = get_connectivity(xyz)
+    neighbors = [[] for i in range(len(xyz))]
+
+    for bond in bonds:
+        a, b = bond
+        neighbors[a].append(b)
+        neighbors[b].append(a)
+
+    return neighbors
+
+def morgan_numbering(xyz):
+    indices = np.zeros(len(xyz)) + 1
+    neighbors = get_neighbors_lists(xyz)
+    num = len(xyz)
+
+    num_unique_indices = 1
+    while True:
+        new_indices = indices.copy()
+        for i in range(num):
+            bonded = neighbors[i]
+            for b in bonded:
+                new_indices[i] += indices[b]
+
+        new_num_unique_indices = len(set(new_indices))
+        if new_num_unique_indices == num_unique_indices:
+            break
+
+        indices = new_indices
+        num_unique_indices = new_num_unique_indices
+
+    return indices
+
+def equivalent_atoms(xyz, algorithm="morgan"):
+    equivalent = []
+    if algorithm == "morgan":
+        indices = morgan_numbering(xyz)
+        for val in set(indices):
+            eqs = np.where(indices == val)[0]
+            if len(eqs) > 1:
+                equivalent.append(eqs)
+    return equivalent
 
 
---
+++ frontend/test_xyz.py
@@ -0,0 +1,99 @@
+import os
+import unittest
+
+from django.test import TestCase, Client
+
+from .libxyz import *
+
+tests_dir = os.path.join('/'.join(__file__.split('/')[:-1]), "tests/")
+SCR_DIR = os.path.join(tests_dir, "scr")
+RESULTS_DIR = os.path.join(tests_dir, "results")
+
+class XyzTests(TestCase):
+    def test_morgan_algorithm1(self):
+        xyz = parse_xyz(os.path.join(tests_dir, 'CH4.xyz'))
+        REF = [5, 2, 2, 2, 2]
+        indices = morgan_numbering(xyz)
+        for ind, i in enumerate(REF):
+            self.assertEqual(indices[ind], i)
+
+    def test_morgan_algorithm2(self):
+        xyz = parse_xyz(os.path.join(tests_dir, 'ethanol.xyz'))
+        REF = [54, 23, 23, 23, 57, 24, 24, 32, 15]
+        indices = morgan_numbering(xyz)
+        for ind, i in enumerate(REF):
+            self.assertEqual(indices[ind], i)
+
+    def equivalent_equivalence(self, test, ref):
+        test_d = {}
+        ref_d = {}
+
+        for eqs in test:
+            l = len(eqs)
+            if l in test_d.keys():
+                test_d[l].append(eqs)
+            else:
+                test_d[l] = [eqs]
+
+        for eqs in ref:
+            l = len(eqs)
+            if l in ref_d.keys():
+                ref_d[l].append(eqs)
+            else:
+                ref_d[l] = [eqs]
+
+        if test_d.keys() != ref_d.keys():
+            return False
+
+        def one_equivalent(arr_arr, arr):
+            for _arr in arr_arr:
+                if np.array_equal(_arr, arr):
+                    return True
+            return False
+
+        for k in test_d.keys():
+            if len(test_d[k]) != len(ref_d[k]):
+                return False
+
+            for arr in test_d[k]:
+                if not one_equivalent(ref_d[k], arr):
+                    return False
+
+        return True
+
+
+    def test_equivalent_atoms1(self):
+        xyz = parse_xyz(os.path.join(tests_dir, 'CH4.xyz'))
+        eqs = equivalent_atoms(xyz)
+        REF = [[1, 2, 3, 4]]
+        self.assertTrue(self.equivalent_equivalence(eqs, REF))
+
+    def test_equivalent_atoms2(self):
+        xyz = parse_xyz(os.path.join(tests_dir, 'ethanol.xyz'))
+        eqs = equivalent_atoms(xyz)
+        REF = [[1, 2, 3], [5, 6]]
+        self.assertTrue(self.equivalent_equivalence(eqs, REF))
+
+    def test_equivalent_atoms3(self):
+        xyz = parse_xyz(os.path.join(tests_dir, 'benzene.xyz'))
+        eqs = equivalent_atoms(xyz)
+        REF = [[0, 1, 2, 3, 4, 5], [6, 7, 8, 9, 10, 11]]
+        self.assertTrue(self.equivalent_equivalence(eqs, REF))
+
+    def test_equivalent_atoms4(self):
+        xyz = parse_xyz(os.path.join(tests_dir, 'propane.xyz'))
+        eqs = equivalent_atoms(xyz)
+        REF = [[5, 6], [0, 7], [1, 2, 3, 8, 9, 10]]
+        self.assertTrue(self.equivalent_equivalence(eqs, REF))
+
+    def test_equivalent_atoms5(self):
+        xyz = parse_xyz(os.path.join(tests_dir, 'Ph2I_cation.xyz'))
+        eqs = equivalent_atoms(xyz)
+        REF = [[4, 12], [3, 5, 13, 14], [0, 2, 15, 17], [1, 19], [9, 10, 16, 18], [6, 8, 20, 21], [7, 22]]
+        self.assertTrue(self.equivalent_equivalence(eqs, REF))
+
+    def test_equivalent_atoms6(self):
+        xyz = parse_xyz(os.path.join(tests_dir, 'elimination_product.xyz'))
+        eqs = equivalent_atoms(xyz)
+        REF = [[10, 11, 12], [0, 1], [4, 5, 6, 7], [3, 9]]
+        self.assertTrue(self.equivalent_equivalence(eqs, REF))
