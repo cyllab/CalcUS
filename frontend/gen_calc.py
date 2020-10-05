0e392a8ef0be9b5176f33084b15a0453a3a289c9
Raphael <raphael.robidas@usherbrooke.ca>
2020-10-05 04:30:45 -0400

    Separate file for test calculation generation

---
+++ frontend/gen_calc.py
@@ -0,0 +1,81 @@
+from .models import *
+
+TESTS_DIR = os.path.join('/'.join(__file__.split('/')[:-1]), "tests/")
+
+def gen_calc(params, profile):
+    step = BasicStep.objects.get(name=params['type'])
+
+    charge = 0
+    multiplicity = 1
+    solvent = "Vacuum"
+    solvation_model = ""
+    solvation_radii = ""
+    basis_set = ""
+    theory_level = ""
+    method = ""
+    additional_command = ""
+    custom_basis_sets = ""
+    density_fitting = ""
+    specifications = ""
+
+    if 'charge' in params.keys():
+        charge = int(params['charge'])
+
+    if 'multiplicity' in params.keys():
+        multiplicity = int(params['multiplicity'])
+
+    if 'solvent' in params.keys():
+        solvent = params['solvent']
+
+    if 'solvation_model' in params.keys():
+        solvation_model = params['solvation_model']
+        if 'solvation_radii' in params.keys():
+            solvation_radii = params['solvation_radii']
+        else:
+            if solvation_model == "SMD":
+                solvation_radii = "Default"
+            if solvation_model in ["PCM", "CPCM"]:
+                solvation_radii = "UFF"
+
+
+    if 'basis_set' in params.keys():
+        basis_set = params['basis_set']
+
+    if 'custom_basis_sets' in params.keys():
+        custom_basis_sets = params['custom_basis_sets']
+
+    if 'density_fitting' in params.keys():
+        density_fitting = params['density_fitting']
+
+    if 'theory_level' in params.keys():
+        theory_level = params['theory_level']
+
+    if 'method' in params.keys():
+        method = params['method']
+
+    if 'additional_command' in params.keys():
+        additional_command = params['additional_command']
+
+    if 'specifications' in params.keys():
+        specifications = params['specifications']
+
+    software = params['software']
+
+    p = Parameters.objects.create(charge=charge, multiplicity=multiplicity, solvent=solvent, solvation_model=solvation_model, solvation_radii=solvation_radii, basis_set=basis_set, theory_level=theory_level, method=method, additional_command=additional_command, custom_basis_sets=custom_basis_sets, density_fitting=density_fitting, specifications=specifications, software=software)
+    with open(os.path.join(TESTS_DIR, params['in_file'])) as f:
+        lines = f.readlines()
+        xyz_structure = ''.join(lines)
+
+    mol = Molecule.objects.create()
+    e = Ensemble.objects.create(parent_molecule=mol)
+    s = Structure.objects.create(xyz_structure=xyz_structure, parent_ensemble=e)
+
+    proj = Project.objects.create()
+    dummy = CalculationOrder.objects.create(project=proj, author=profile)
+    calc = Calculation.objects.create(structure=s, step=step, parameters=p, order=dummy)
+
+    if 'constraints' in params.keys():
+        calc.constraints = params['constraints']
+
+    return calc
+
