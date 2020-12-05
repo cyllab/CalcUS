3adc41ac1e8f8e2d9bc7e24b01aa0ac08e9da7ce
Raphael Robidas <raphael.robidas@usherbrooke.ca>
2020-12-05 11:34:47 -0500

    Update to scripts

--- scripts/run_web.sh
+++ scripts/run_web.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 su -m calcus -c "python manage.py makemigrations"
+su -m calcus -c "python manage.py makemigrations frontend"
 su -m calcus -c "python manage.py migrate"
 su -m calcus -c "gunicorn calcus.wsgi:application --bind 0.0.0.0:8000"
