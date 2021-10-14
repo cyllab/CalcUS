b212663c19850db60bc6ffee8d473bd464f2a347
Raphael <raphael.robidas@usherbrooke.ca>
2021-10-14 14:29:16 -0400

    Autocreate objects now

--- scripts/run_web.sh
+++ scripts/run_web.sh
@@ -2,4 +2,6 @@
 python manage.py makemigrations
 python manage.py makemigrations frontend
 python manage.py migrate
+python manage.py init_static_obj
+
 gunicorn calcus.wsgi:application --bind 0.0.0.0:8000 --access-logfile=- --error-logfile=-
