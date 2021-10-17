719e3fe7989cf06e10d0b520ea41f201235b0948
Raphael <raphael.robidas@usherbrooke.ca>
2021-10-16 21:10:18 -0400

    Tweaks

--- frontend/management/commands/wipe.py
+++ frontend/management/commands/wipe.py
@@ -1,10 +1,30 @@
+'''
+This file of part of CalcUS.
+
+Copyright (C) 2020-2021 Raphaël Robidas
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+'''
+
+
 import glob
 import os
 from django.core.management.base import BaseCommand
 from frontend.models import *
 
 try:
-    os.environ['LAB_TEST']
+    os.environ['CALCUS_TEST']
 except:
     is_test = False
 else:
