65274dbe51e7e980c10c940d6b7b53c0c6b3cb66
Raphaël Robidas <Raphael.Robidas@USherbrooke.ca>
2023-02-06 18:45:22 -0500

    Major refactor in the launch page

--- frontend/templates/frontend/dynamic/load_params.js
+++ frontend/templates/frontend/dynamic/load_params.js
@@ -1,48 +1,48 @@
 if ("{{ params.software }}" != "Unknown" && "{{ params.software }}" != "Open Babel") {
-	solvent = document.getElementById("calc_solvent");
+	solvent = document.getElementsByName("calc_solvent")[0];
 	solvent.value = "{{ params.solvent }}";
 
-	solvation_model = document.getElementById("calc_solvation_model");
+	solvation_model = document.getElementsByName("calc_solvation_model")[0];
 	solvation_model.value = "{{ params.solvation_model }}";
 
-	solvation_radii = document.getElementById("calc_solvation_radii");
+	solvation_radii = document.getElementsByName("calc_solvation_radii")[0];
 	solvation_radii.value = "{{ params.solvation_radii }}";
 
-	software = document.getElementById("calc_software");
+	software = document.getElementsByName("calc_software")[0];
 	software.value = "{{ params.software }}";
 
-	theory_level = document.getElementById("calc_theory_level");
+	theory_level = document.getElementsByName("calc_theory_level")[0];
 	theory_level.value = "{{ params.theory_level }}";
 
-	basis_set = document.getElementById("calc_basis_set");
+	basis_set = document.getElementsByName("calc_basis_set")[0];
 	basis_set.value = "{{ params.basis_set }}";
 
 	{% if params.method == "PBEh-3c" %}
-		pbeh3c = document.getElementById("pbeh3c");
+		pbeh3c = document.getElementsByName("pbeh3c")[0];
 		pbeh3c.checked = true;
 	{% elif params.method == "HF-3c" %}
-		hf3c = document.getElementById("hf3c");
+		hf3c = document.getElementsByName("hf3c")[0];
 		hf3c.checked = true;
 	{% else %}
-		func = document.getElementById("calc_functional");
+		func = document.getElementsByName("calc_functional")[0];
 		func.value = "{{ params.method }}";
 	{% endif %}
 	
 	{% if load_charge %}
-		charge = document.getElementById("calc_charge");
+		charge = document.getElementsByName("calc_charge")[0];
 		charge.value = "{{ params.charge }}";
-		mult = document.getElementById("calc_multiplicity");
+		mult = document.getElementsByName("calc_multiplicity")[0];
 		mult.value = "{{ params.multiplicity }}";
 	{% endif %}
 
-	df = document.getElementById("calc_df");
+	df = document.getElementsByName("calc_df")[0];
 	df.value = "{{ params.density_fitting }}";
 
-	bs = document.getElementById("calc_custom_bs");
+	bs = document.getElementsByName("calc_custom_bs")[0];
 	bs.value = "{{ params.custom_basis_sets }}";
 
     if("{{ params.software }}" != "xtb") {
-        specifications = document.getElementById("calc_specifications");
+        specifications = document.getElementsByName("calc_specifications")[0];
         specifications.value = "{{ params.specifications }}";
     }
 }
