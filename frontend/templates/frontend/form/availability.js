0d0fbda75e1ac8f4d80b9f4e5325e5fe0dddb75a
Raphaël Robidas <Raphael.Robidas@USherbrooke.ca>
2023-07-18 20:31:15 -0400

    Improved the option filtering for batch calculations
    Former-commit-id: af88c7447e5ba429f860e3f382a4b4d3ae15c77e

--- frontend/templates/frontend/form/availability.js
+++ frontend/templates/frontend/form/availability.js
@@ -1,55 +1,5 @@
 var master_list_params = ["software", "type", "solvent", "solvation_model", "solvation_radii", "theory_level", "driver", "resource"];
 var master_options = {
-    {% if interface == "advanced" %}
-    "software": {
-        "Gaussian": {
-            "type": ["NMR Prediction", "Geometrical Optimisation", "TS Optimisation", "Frequency Calculation", "Constrained Optimisation", "Single-Point Energy", "UV-Vis Calculation"],
-            "solvation_model": ["PCM", "CPCM", "SMD"],
-            "se_method": ["AM1", "PM3", "PM6", "PM7"],
-            "solvation_radii": ["SMD18", "Default", "UFF", "UA0", "UAHF", "UAKS", "Pauling", "Bondi"],
-            "theory_level": ["semiempirical", "hf", "dft"],
-            "driver": ["Gaussian"]
-        },
-        "ORCA": {
-            "type": ["NMR Prediction", "Geometrical Optimisation", "TS Optimisation", "Frequency Calculation", "Constrained Optimisation", "Single-Point Energy", "MO Calculation", "Minimum Energy Path"],
-            "solvation_model": ["CPCM", "SMD"],
-            "se_method": ["AM1", "PM3"],
-            "solvation_radii": ["SMD18", "Default"],
-            "theory_level": ["semiempirical", "hf", "dft", "mp2"],
-            "driver": ["ORCA", "Pysisyphus"]
-        },
-        "xtb": {
-            "type": ["Geometrical Optimisation", "TS Optimisation", "Frequency Calculation", "Constrained Optimisation", "Single-Point Energy", "UV-Vis Calculation", "Conformational Search", "Constrained Conformational Search", "Minimum Energy Path"],
-            "solvation_model": ["ALPB", "GBSA"],
-            "solvation_radii": ["Default"],
-            "theory_level": ["xtb"],
-            "driver": ["xtb", "Pysisyphus", "ORCA"]
-        },
-        "NWChem": {
-            "type": ["Single-Point Energy", "MO Calculation", "ESP Calculation", "Geometrical Optimisation", "Frequency Calculation"],
-            "theory_level": ["hf", "dft"],
-            "driver": ["NWChem"],
-            "solvation_model": [],
-        },
-    },
-    {% endif %}
-    "solvation_model": {
-        "SMD": {
-            "solvation_radii": ["Default", "SMD18"],
-        },
-        "PCM": {
-            "solvation_radii": ["Default", "UFF", "UA0", "UAHF", "UAKS", "Pauling", "Bondi"],
-        },
-        "CPCM": {
-            "solvation_radii": ["Default", "UFF", "UA0", "UAHF", "UAKS", "Pauling", "Bondi"],
-        },
-        "GBSA": {
-            "solvation_radii": ["Default"],
-        },
-        "ALPB": {
-            "solvation_radii": ["Default"],
-        }
-    },
     "type": {
         "Minimum Energy Path": {
             "software": ["xtb", "ORCA"],
@@ -102,12 +52,105 @@ var master_options = {
             "theory_level": ["xtb"]
         }
     },
+    "software": {
+        "Gaussian": {
+            "type": ["NMR Prediction", "Geometrical Optimisation", "TS Optimisation", "Frequency Calculation", "Constrained Optimisation", "Single-Point Energy", "UV-Vis Calculation"],
+            "solvation_model": ["PCM", "CPCM", "SMD"],
+            "se_method": ["AM1", "PM3", "PM6", "PM7"],
+            "solvation_radii": ["SMD18", "Default", "UFF", "UA0", "UAHF", "UAKS", "Pauling", "Bondi"],
+            "theory_level": ["semiempirical", "hf", "dft"],
+            "driver": ["Gaussian"]
+        },
+        "ORCA": {
+            "type": ["NMR Prediction", "Geometrical Optimisation", "TS Optimisation", "Frequency Calculation", "Constrained Optimisation", "Single-Point Energy", "MO Calculation", "Minimum Energy Path"],
+            "solvation_model": ["CPCM", "SMD"],
+            "se_method": ["AM1", "PM3"],
+            "solvation_radii": ["SMD18", "Default"],
+            "theory_level": ["semiempirical", "hf", "dft", "mp2"],
+            "driver": ["ORCA", "Pysisyphus"]
+        },
+        "xtb": {
+            "type": ["Geometrical Optimisation", "TS Optimisation", "Frequency Calculation", "Constrained Optimisation", "Single-Point Energy", "UV-Vis Calculation", "Conformational Search", "Constrained Conformational Search", "Minimum Energy Path"],
+            "solvation_model": ["ALPB", "GBSA"],
+            "solvation_radii": ["Default"],
+            "theory_level": ["xtb"],
+            "driver": ["xtb", "Pysisyphus", "ORCA"]
+        },
+        "NWChem": {
+            "type": ["Single-Point Energy", "MO Calculation", "ESP Calculation", "Geometrical Optimisation", "Frequency Calculation"],
+            "theory_level": ["hf", "dft"],
+            "driver": ["NWChem"],
+            "solvation_model": ["COSMO", "SMD"],
+        },
+    },
+    "solvation_model": {
+        "SMD": {
+            "solvation_radii": ["Default", "SMD18"],
+        },
+        "PCM": {
+            "solvation_radii": ["Default", "UFF", "UA0", "UAHF", "UAKS", "Pauling", "Bondi"],
+        },
+        "CPCM": {
+            "solvation_radii": ["Default", "UFF", "UA0", "UAHF", "UAKS", "Pauling", "Bondi"],
+        },
+        "GBSA": {
+            "solvation_radii": ["Default"],
+        },
+        "ALPB": {
+            "solvation_radii": ["Default"],
+        },
+        "COSMO": {
+            "solvation_radii": ["Default"],
+        }
+    },
     "resource": {
         "Local": {
             "driver": [{% if "Gaussian" in packages %}"Gaussian", {% endif %}{% if "ORCA" in packages %}"ORCA", {% endif %}"Pysisyphus", "xtb", "NWChem"],
             "software": [{% if "Gaussian" in packages %}"Gaussian", {% endif %}{% if "ORCA" in packages %}"ORCA", {% endif %} "xtb", "NWChem"]
         }
+    }{% if is_batch %},
+    /*
+     * Inverse restrictions (children restricting parents)
+     * Should only be used when the children can be toggled "on" or "off", 
+     * so that the options aren't deadlocked (children preventing the parents from changing, 
+     * while the parents prevent the children from changing).
+     * */
+    "theory_level": {
+        "xtb": {
+            "software": ["xtb"]
+        },
+        "semiempirical": {
+            "software": ["Gaussian", "ORCA"]
+        },
+        "hf": {
+            "software": ["Gaussian", "ORCA", "NWChem"]
+        },
+        "dft": {
+            "software": ["Gaussian", "ORCA", "NWChem"]
+        },
+        "mp2": {
+            "software": ["ORCA"]
+        }
+
+    },
+    "driver": {
+        "xtb": {
+            "software": ["xtb"]
+        },
+        "ORCA": {
+            "software": ["xtb", "ORCA"]
+        },
+        "Gaussian": {
+            "software": ["Gaussian"]
+        },
+        "Pysisyphus": {
+            "software": ["xtb", "ORCA"]
+        },
+        "NWChem": {
+            "software": ["NWChem"]
+        }
     }
+    {% endif %}
 }
 var list_available_elements = ["df", "pbeh3c", "hf3c", "se_method", "functional_field", "xtb_method", "basis_set_field", "custom_bs", "aux_file_structure", "aux_structure", "solvation_model", "solvation_radii", "constraints"];
 var master_available = {
@@ -115,8 +158,9 @@ var master_available = {
         "Gaussian": ["df", "se_method", "functional_field", "basis_set_field", "custom_bs", "solvation_model", "solvation_radii", "constraints"],
         "ORCA": ["hf3c", "pbeh3c", "se_method", "functional_field", "basis_set_field", "custom_bs", "solvation_model", "solvation_radii", "constraints", "aux_file_structure", "aux_structure"],
         "xtb": ["aux_file_structure", "aux_structure", "solvation_model", "solvation_radii", "constraints", "xtb_method"],
-        "NWChem": ["functional_field", "basis_set_field"],
+        "NWChem": ["functional_field", "basis_set_field", "solvation_model", "solvation_radii"],
     },
+
     "theory_level": {
         "xtb": ["aux_file_structure", "aux_structure", "solvation_model", "solvation_radii", "constraints", "xtb_method"],
         "semiempirical": ["aux_file_structure", "aux_structure", "se_method", "solvation_model", "solvation_radii", "constraints"],
@@ -143,6 +187,14 @@ var master_available = {
     }
 }
 
+var parameters_dependencies = {
+    "method": ["theory_level"],
+    "basis_set": ["theory_level"],
+    "driver": ["software"],
+    "solvation_radii": ["solvent"],
+    "solvation_model": ["solvent"],
+}
+
 function refresh_availabilities() {
     // Since some parameters that will change can influence which parameters should be visible, we have to execute the function twice
     _refresh_availabilities();
@@ -158,9 +210,21 @@ function _refresh_availabilities() {
 
     master_list_params.forEach(p => {
         el = document.getElementsByName("calc_"+p)[0];
+        
         // Some elements can be omitted when generating the page depending on the parameters 
         // (e.g., launching from ensemble)
         if(el != undefined) {
+            {% if is_batch %}
+            // We only want to hide options if the parameter is chosen as fixed.
+            // Ignore the cases where the structure is different (outside the fixed parameters section)
+            try {
+                parent_row = el.closest(".columns");
+                checkbox = parent_row.querySelector(".column.is-1 > input");
+                if(!checkbox.checked || checkbox.disabled)
+                    return
+            }
+            catch(TypeError) {}
+            {% endif %}
             choice = el.value;
             if (p in master_options && choice in master_options[p]) {
                 for (const key2 in master_options[p][choice]) {
@@ -174,8 +238,11 @@ function _refresh_availabilities() {
                             }
                         }
                     }
-                    catch(TypeError) {
-                        console.log("Did not find an element with name calc_" + key2);
+                    catch(e) {
+                        if(e instanceof TypeError || e instanceof ReferenceError)
+                            console.log("Did not find an element with name calc_" + key2);
+                        else
+                            console.log("Expected error while accessing calc_" + key2 + ": " + String(e))
                     }
                 }
             }
@@ -184,16 +251,23 @@ function _refresh_availabilities() {
                     key = list_available_elements[ind];
                     try {
                         el2 = document.getElementById("calc_"+key);
+                        row = document.getElementById("row_"+key);
                         if(el2 != undefined) {
                             if(!master_available[p][choice].includes(key)) {
                                 el2.style.display = "none";
                                 el2.classList.add("unavailable");
+                                row.style.display = "none";
+                                row.classList.add("unavailable");
                             }
                         }
                     }
-                    catch(TypeError) {
-                        console.log("Did not find an element with name calc_" + key2);
+                    catch(e) {
+                        if(e instanceof TypeError || e instanceof ReferenceError)
+                            console.log("Did not find an element with name calc_" + key);
+                        else
+                            console.log("Expected error while accessing calc_" + key + ": " + String(e))
                     }
+ 
                 }
             }
         }
@@ -226,6 +300,21 @@ function _refresh_availabilities() {
             }
         });
     });
+
+    {% if is_batch %}
+    for (const key in parameters_dependencies) {
+        el = document.getElementsByName(key + "_fixed")[0];
+        el.removeAttribute("disabled");
+        for (const dep_key in parameters_dependencies[key]) {
+            set = document.getElementsByName(parameters_dependencies[key][dep_key] + "_fixed")[0].checked;
+            if(!set) {
+                el.setAttribute("disabled", true);
+                break;
+            }
+        }
+
+    }
+    {% endif %}
 }
 
 function set_availables() {
