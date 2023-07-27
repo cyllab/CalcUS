567606443ebf77205b6cf0cad0c0c04e2ec86923
Raphaël Robidas <Raphael.Robidas@USherbrooke.ca>
2023-07-27 15:17:50 -0400

    Basic batch calculations frontend based on Anchal's work
    Former-commit-id: c988df89e223f16d0b5562674834fe2e5a70e863

--- frontend/views.py
+++ frontend/views.py
@@ -5847,9 +5847,3 @@ def stripe_config(request):
     if request.method == "GET":
         stripe_config = {"publicKey": settings.STRIPE_PUBLISHABLE_KEY}
         return JsonResponse(stripe_config, safe=False)
-
-
-
-    
-
-      
