87361ffeee936ce7a1b7947820d21bd9ee87e6f0
Raphaël Robidas <Raphael.Robidas@USherbrooke.ca>
2023-08-14 14:01:09 -0400

    Fixed the basic batch calculations code

--- frontend/urls.py
+++ frontend/urls.py
@@ -224,6 +224,7 @@ urlpatterns = [
     path("delete_order/", views.delete_order, name="delete_order"),
     path("delete_folder/", views.delete_folder, name="delete_folder"),
     path("batch_calculations/", views.batch_calculations, name="batch_calculations"),
+    path("receive_params/", views.receive_params, name="receive_params"),
     path("flowchart/", views.flowchart, name="flowchart"),
     path("create_flowchart/", views.create_flowchart, name="create_flowchart"),
     path("submit_flowchart/", views.submit_flowchart, name="submit_flowchart"),
