aef6ae7c3da4409906ec91f1091f0655e52d2884
Raphaël Robidas <Raphael.Robidas@USherbrooke.ca>
2023-08-20 21:11:05 -0400

    Improved the web 3D editor (support for bimolecular systems and essential manipulations)
    Former-commit-id: 8f076687b379b82c347d770c053b22883763ffa0

--- frontend/templates/frontend/form/input_widgets_head.js
+++ frontend/templates/frontend/form/input_widgets_head.js
@@ -21,3 +21,34 @@ function load_nih(btn) {
         $(btn).removeClass("is-loading");
     });
 }
+
+function add_3D() {
+    let mol = ChemDoodle.writeMOL(sketcher.getMolecule());
+    document.getElementById("add_3D_btn").classList.add("is-loading");
+    $.ajax({
+        method: "POST",	 	 
+        url: "/gen_3D/",
+        headers: {
+            "X-CSRFToken": '{{ csrf_token }}',
+        },
+
+        data: {'mol': mol},
+        statusCode: {
+            404: function(data) {
+                $("#3d_msg").html("Error while generating structure");
+            },
+            200: function(data) {
+                $("#3d_msg").html("");
+                let mol_read = ChemDoodle.readXYZ(data);
+                ChemDoodle.relabel(mol_read);
+                viewer.addMolecule(mol_read);	
+                document.getElementById("add_3D_btn").classList.remove("is-loading");
+                
+                Jmol.script(editor, 'load APPEND inline "' + data + '"');
+            }
+        }
+    }).always(function() {
+        document.getElementById("gen_3D_btn").classList.remove("is-loading");
+    });
+
+}
