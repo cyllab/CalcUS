c309f9a2f4af1922d15cd1c84e8bd9869c4ac031
Raphaël Robidas <raphael.robidas@usherbrooke.ca>
2024-02-04 09:19:23 -0500

    Misc update

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose -f test-compose.yml -f test-compose.override.yml run web /calcus/scripts/run_coverage_tests.sh
+docker compose -f test-compose.yml -f test-compose.override.yml run web /calcus/scripts/run_coverage_tests.sh
