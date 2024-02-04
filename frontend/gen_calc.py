ca048e69cc0935c5bb2fdd81501cb97bc1bc19fe
Lint Action <lint-action@samuelmeuli.com>
2024-02-04 13:52:34 +0000

    Code style standardization
    Former-commit-id: 80269e72fb1aba303f4f80e1b835b37f4dabd9f7

--- frontend/gen_calc.py
+++ frontend/gen_calc.py
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-
 from .models import *
 from .tasks import generate_xyz_structure
 
--- frontend/management/commands/init_static_obj.py
+++ frontend/management/commands/init_static_obj.py
@@ -17,7 +17,6 @@ You should have received a copy of the GNU General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-
 import os
 from django.core.management.base import BaseCommand
 from django.conf import settings
