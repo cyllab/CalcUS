6e8a2efc403d7b0a2595a45cb339284f173efb39
Raphaël Robidas <raphael.robidas@usherbrooke.ca>
2024-04-14 09:42:03 -0400

    Updated CREST

--- scripts/extract_xtb.py
+++ scripts/extract_xtb.py
@@ -3,10 +3,10 @@ import subprocess
 import shlex
 
 if not os.path.isdir("/binaries/xtb/xtb"):
-    subprocess.call(shlex.split("tar xvfj xtb_suite.tar.bz2"), cwd="/binaries/xtb/")
+    subprocess.call(shlex.split("tar xvfJ xtb_suite.tar.xz"), cwd="/binaries/xtb/")
     subprocess.call(
         shlex.split("chmod +x crest stda xtb4stda xtb/bin/xtb"), cwd="/binaries/xtb/"
     )
 if not os.path.isdir("/binaries/xtb/Multiwfn"):
-    subprocess.call(shlex.split("tar xvfj Multiwfn.tar.bz2"), cwd="/binaries/xtb/")
+    subprocess.call(shlex.split("tar xvfJ Multiwfn.tar.xz"), cwd="/binaries/xtb/")
     subprocess.call(shlex.split("chmod +x Multiwfn"), cwd="/binaries/xtb/")
