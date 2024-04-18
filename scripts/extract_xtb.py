62993a375bf23acf3fccedb1d8af5b1da6d79655
Raphaël Robidas <raphael.robidas@usherbrooke.ca>
2024-04-18 11:40:22 -0400

    Fixed Multiwfn

--- scripts/extract_xtb.py
+++ scripts/extract_xtb.py
@@ -7,6 +7,8 @@ if not os.path.isdir("/binaries/xtb/xtb"):
     subprocess.call(
         shlex.split("chmod +x crest stda xtb4stda xtb/bin/xtb"), cwd="/binaries/xtb/"
     )
+    os.remove("/binaries/xtb/xtb_suite.tar.xz")
 if not os.path.isdir("/binaries/xtb/Multiwfn"):
-    subprocess.call(shlex.split("tar xvfJ Multiwfn.tar.xz"), cwd="/binaries/xtb/")
+    subprocess.call(shlex.split("tar xvfj Multiwfn.tar.bz2"), cwd="/binaries/xtb/")
     subprocess.call(shlex.split("chmod +x Multiwfn"), cwd="/binaries/xtb/")
+    os.remove("/binaries/xtb/Multiwfn.tar.bz2")
