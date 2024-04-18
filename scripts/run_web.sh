1fc0aaf80bcd6555d3f68da31eecee6597baa67e
Raphaël Robidas <raphael.robidas@usherbrooke.ca>
2024-04-18 12:05:05 -0400

    Updated cloud image

--- scripts/run_web.sh
+++ scripts/run_web.sh
@@ -4,4 +4,4 @@ python scripts/wait_for_postgres.py
 ./scripts/migrate.sh
 python manage.py check_su
 
-gunicorn calcus.wsgi:application --bind 0.0.0.0:8000 --access-logfile=- --error-logfile=- --reload --timeout $GUNICORN_TIMEOUT --keep-alive $GUNICORN_TIMEOUT
+python -m gunicorn calcus.wsgi:application --bind 0.0.0.0:8000 --access-logfile=- --error-logfile=- --reload --timeout $GUNICORN_TIMEOUT --keep-alive $GUNICORN_TIMEOUT
