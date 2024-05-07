09d5b5e2f05e3885e87e915b283e27e6c51387cf
Raphaël Robidas <raphael.robidas@usherbrooke.ca>
2024-05-07 10:38:05 -0400

    Fixed script
    Former-commit-id: e63103ff6ebae62befc607f4e982f6171a8841ba

--- scripts/extract_xtb.py
+++ scripts/extract_xtb.py
@@ -8,7 +8,7 @@ if not os.path.isdir("/binaries/xtb"):
         shlex.split("chmod +x crest stda xtb4stda xtb/bin/xtb"),
         cwd="/binaries/",
     )
-    os.remove("/binaries/xtb/xtb_suite.tar.xz")
+    os.remove("/binaries/xtb_suite.tar.xz")
 if not os.path.isdir("/binaries/Multiwfn"):
     subprocess.call(shlex.split("tar xvfj Multiwfn.tar.bz2"), cwd="/binaries/")
     subprocess.call(shlex.split("chmod +x Multiwfn"), cwd="/binaries/")
