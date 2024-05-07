19eb8a27787ae3574c0a4e80eae039e8b83bb04b
Raphaël Robidas <raphael.robidas@usherbrooke.ca>
2024-05-07 11:11:39 -0400

    More debugging
    Former-commit-id: d4662960e24748ae49f76366f3e9272262d97334

--- frontend/tasks.py
+++ frontend/tasks.py
@@ -538,10 +538,11 @@ def system(
                 t = subprocess.Popen(shlex.split(command), stdout=stream, stderr=stream)
 
         except FileNotFoundError as e:
-            targetstr = ",".join(glob.glob("/binaries/xtb/*"))
+            targetstr = ",".join(glob.glob("/binaries/*"))
             logger.error(
                 f'Could not run command "{command}" - executable not found (msg: {str(e)}), os.environ is {str(os.environ)} and target dir contains {targetstr}'
             )
+            os.system("ls /")
             calc.error_message = f"{command.split()[0]} is not found"
             calc.date_finished = timezone.now()
             calc.save()
